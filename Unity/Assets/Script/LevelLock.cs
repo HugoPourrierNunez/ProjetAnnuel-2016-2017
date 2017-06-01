e1172f3b4bf8922e9a3af1c64ab039fc7137278c
Maxime Rignault <maximerignault@gmail.com>
2017-06-01 19:29:04 +0200

    Bugfixed levels locks

--- Unity/Assets/Script/LevelLock.cs
+++ Unity/Assets/Script/LevelLock.cs
@@ -11,15 +11,15 @@ public class LevelLock : MonoBehaviour
     [SerializeField]
     GameObject[] _locks;
 
-    [SerializeField]
-    GameManager _gameManager;
+    //[SerializeField]
+    //GameManager _gameManager;
 
     private int _unlockLvl;
 
     void OnEnable ()
     {
-        _unlockLvl = PlayerPrefs.GetInt("max_level");
-        for(int i = _unlockLvl + 1; i < 5; ++i)
+        _unlockLvl = PlayerPrefs.GetInt("max_level", 1);
+        for(int i = _unlockLvl; i < 5; ++i)
         {
             _levelsBtn[i].interactable = false;
             _locks[i].SetActive(true);
