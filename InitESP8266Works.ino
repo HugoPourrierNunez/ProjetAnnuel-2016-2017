9314049d8859e88ad86e9a17c1a5d5f37a683b28
Anthony BAZELLE <a.bazelle1@laposte.net>
2018-04-05 22:23:58 +0200

    Arduino code a jour

--- InitESP8266Works.ino
+++ InitESP8266Works.ino
@@ -3,16 +3,18 @@
 
     SoftwareSerial ESP8266(10, 12); // 10 TX 12 RX
 
-    String NomduReseauWifi = ""; // Garder les guillements
-    String MotDePasse      = ""; // Garder les guillements
+    String NomduReseauWifi = "TP-LINK_BACA"; // Garder les guillements
+    String MotDePasse      = "EasyGlove"; // Garder les guillements
     
     int vibThumb = 3;
     int vibIndex = 5;
-    int vibMiddle = 6;
-    int vibRing = 9;
-    int vibPinky = 11;
+    int vibMiddle = 9;
+    int vibRing = 11;
+    int vibPinky = 6;
 
-    int handId = 2;
+    //int handId = 2;
+    
+      int temp = 4000;
 
 
     int sensorValue = 0;
@@ -25,7 +27,7 @@
       
       ESP8266.begin(115200);
       envoieAuESP8266("AT+CIOBAUD=9600");
-      recoitDuESP8266(4000);
+      recoitDuESP8266(temp);
       
       pinMode(vibThumb, OUTPUT);
       pinMode(vibIndex, OUTPUT);
@@ -43,7 +45,11 @@
     {
        while(ESP8266.available())
        {   
-          recoitDuESP8266(1000); 
+          /*analogWrite(vibIndex, 0);
+          delay(1000);
+          Serial.print("\nVIB INDEX\n");
+          analogWrite(vibIndex, 0);*/ 
+          recoitDuESP8266(0); 
        }   
     }
     /****************************************************************/
@@ -55,22 +61,22 @@
       Serial.println("**************** DEBUT DE L'INITIALISATION ***************");
       Serial.println("**********************************************************");  
       envoieAuESP8266("AT");
-      recoitDuESP8266(2000);
+      recoitDuESP8266(temp);
       Serial.println("**********************************************************");
       envoieAuESP8266("AT+CWMODE=3");
-      recoitDuESP8266(5000);
+      recoitDuESP8266(temp);
       Serial.println("**********************************************************");
       envoieAuESP8266("AT+CWJAP=\""+ NomduReseauWifi + "\",\"" + MotDePasse +"\"");
-      recoitDuESP8266(10000);
+      recoitDuESP8266(temp);
       Serial.println("**********************************************************");
       envoieAuESP8266("AT+CIFSR");
-      recoitDuESP8266(1000);
+      recoitDuESP8266(temp);
       Serial.println("**********************************************************");
       envoieAuESP8266("AT+CIPMUX=1");   
-      recoitDuESP8266(1000);
+      recoitDuESP8266(temp);
       Serial.println("**********************************************************");
       envoieAuESP8266("AT+CIPSERVER=1,80");
-      recoitDuESP8266(1000);
+      recoitDuESP8266(temp);
       Serial.println("**********************************************************");
       Serial.println("***************** INITIALISATION TERMINEE ****************");
       Serial.println("**********************************************************");
@@ -91,77 +97,68 @@
     {
       String reponse = "";
       long int time = millis();
-      while( (time+timeout) > millis())
+      long int timePlus = time + timeout;
+       
+      while(ESP8266.available())
       {
-        while(ESP8266.available())
-        {
-          char c = ESP8266.read();
-          reponse+=c;
-        }
+        char c = ESP8266.read();
+        reponse+=c;
       }
-
+      
       int index1 = reponse.indexOf(":GET");
       
       if(index1 != -1)
       {
         String valueStr = reponse.substring(index1 + 6, reponse.indexOf("HTTP/") - 1);
-        Serial.print(valueStr); 
-
-        int handIndex = valueStr.substring(0, valueStr.indexOf("?")).toInt();
-        Serial.print("\nHand Index : ");
-        Serial.print(handIndex);
-        if(handIndex == handId)
-        {
-          String rest = valueStr.substring(valueStr.indexOf("?") + 1);
-          int fingerIndex = rest.substring(0, rest.indexOf("?")).toInt();
-          rest = rest.substring(rest.indexOf("?") + 1);
-          int vibIntensity = rest.substring(0, rest.indexOf("?")).toInt();
-          rest = rest.substring(rest.indexOf("?") + 1);
-          int timeVib = rest.toInt();
-          
-          Serial.print("\nFinger Index : ");
-          Serial.print(fingerIndex);
-          Serial.print("\nVib Intensity : ");
-          Serial.print(vibIntensity);
-          Serial.print("\nTime vib : ");
-          Serial.print(timeVib);
-
-          switch(fingerIndex)
-          {
-            case 1:
-              Serial.print("\nVIB THUMB\n");
-              analogWrite(vibThumb, vibIntensity); 
-              delay(timeVib);
-              analogWrite(vibThumb, 0);
-              break;
-            case 2:
-              analogWrite(vibIndex, vibIntensity);
-              delay(timeVib);
-              Serial.print("\nVIB INDEX\n");
-              analogWrite(vibIndex, 0); 
-              break;
-            case 3:
-            Serial.print("\nVIB MIDDLE\n");
-              analogWrite(vibMiddle, vibIntensity); 
-              delay(timeVib);
-              analogWrite(vibMiddle, 0);
-              break;
-            case 4:
-              Serial.print("\nVIB RING\n");
-              analogWrite(vibRing, vibIntensity); 
-              delay(timeVib);
-              analogWrite(vibRing, 0);
-              break;
-            case 5:
-              Serial.print("\nVIB PINKY\n");
-              analogWrite(vibPinky, vibIntensity); 
-              delay(timeVib);
-              analogWrite(vibPinky, 0);
-              break;
-          }
-        }
+        //Serial.print(valueStr); 
+        
+        int fingerIndex = valueStr.substring(0, valueStr.indexOf("?")).toInt();
+        String rest = valueStr.substring(valueStr.indexOf("?") + 1);
+        int vibIntensity = rest.substring(0, rest.indexOf("?")).toInt();
+        rest = rest.substring(rest.indexOf("?") + 1);
+        int timeVib = rest.toInt();
+        
+        analogWrite(fingerIndex, vibIntensity); 
+        delay(timeVib);
+        analogWrite(fingerIndex, 0);
       }
       
-      Serial.print("Leaving from recoitDuESP8266\n"); 
+      //Serial.print("Leaving from recoitDuESP8266\n"); 
+      Serial.print(reponse);   
+    }
+
+    void recoitDuESP8266Wait(const int timeout)
+    {
+      String reponse = "";
+      long int time = millis();
+      long int timePlus = time + timeout;
+
+      while(time+timeout > millis())
+      {
+      while(ESP8266.available())
+      {
+        char c = ESP8266.read();
+        reponse+=c;
+      }
+      }
+      int index1 = reponse.indexOf(":GET");
+      
+      if(index1 != -1)
+      {
+        String valueStr = reponse.substring(index1 + 6, reponse.indexOf("HTTP/") - 1);
+        //Serial.print(valueStr); 
+        
+        int fingerIndex = valueStr.substring(0, valueStr.indexOf("?")).toInt();
+        String rest = valueStr.substring(valueStr.indexOf("?") + 1);
+        int vibIntensity = rest.substring(0, rest.indexOf("?")).toInt();
+        rest = rest.substring(rest.indexOf("?") + 1);
+        int timeVib = rest.toInt();
+        
+        analogWrite(fingerIndex, vibIntensity); 
+        delay(timeVib);
+        analogWrite(fingerIndex, 0);
+      }
+      
+      //Serial.print("Leaving from recoitDuESP8266\n"); 
       Serial.print(reponse);   
     }
